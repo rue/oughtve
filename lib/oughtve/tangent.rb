8be3975cfe977f0ede2b75970b98683c7778fab1
Eero Saynatkari <oughtve@projects.kittensoft.org>
2008-11-24 20:45:30 +0200

    Minor refactor of Oughtve.strike.

--- lib/oughtve/tangent.rb
+++ lib/oughtve/tangent.rb
@@ -118,29 +118,28 @@ module Oughtve
   # Used to mark a note completed (or just get rid of it.)
   #
   def self.strike(parameters)
-    if parameters.serial
-      verse = Verse.get! parameters.serial
-      raise ArgumentError, "Already stricken: #{parameters.serial}" if verse.stricken
+    verse = if parameters.serial
+              Verse.get! parameters.serial
+            else
+              tangent = tangent_for parameters
+              candidates =  tangent.current_chapter.verses.select {|v|
+                              parameters.regexp =~ v.text
+                            }
 
-      verse.stricken = parameters.time
-      verse.save
-    else
-      tangent = tangent_for parameters
-      candidates = tangent.current_chapter.verses.select {|v| parameters.regexp =~ v.text }
+              if candidates.size < 1
+                raise ArgumentError, "No match for #{parameters.regexp.inspect}!"
+              elsif candidates.size > 1
+                # TODO: Show matches
+                raise ArgumentError, "Ambiguous #{parameters.regexp.inspect}!"
+              end
 
-      if candidates.size < 1
-        raise ArgumentError, "No match for #{parameters.regexp.inspect}!"
-      elsif candidates.size > 1
-        # TODO: Show matches
-        raise ArgumentError, "Ambiguous #{parameters.regexp.inspect}!"
-      end
+              candidates.first
+            end
 
-      verse = candidates.first
-      raise ArgumentError, "Already stricken: #{parameters.serial}" if verse.stricken
+    raise ArgumentError, "Already stricken: #{parameters.serial}" if verse.stricken
 
-      verse.stricken = parameters.time
-      verse.save
-    end
+    verse.stricken = parameters.time
+    verse.save
   end
 
 
