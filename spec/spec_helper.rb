6c97bc333d41460db62edcbc693f4d562d70f025
Eero Saynatkari <projects@kittensoft.org>
2012-08-08 16:13:15 +0300

    Kill the spec runner in favor of using database_cleaner, restructure spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,40 @@ $LOAD_PATH.unshift "#{File.dirname(__FILE__)}/../lib"
 
 require "oughtve"
 
-# Sandbox, see spec.rb.
-Oughtve.run %w[ --bootstrap ]
+require "minitest/autorun"
+
+require "database_cleaner"
+require "dm-transactions"
+
+require "fakefs/safe"
+
+# Hide some warnings explicitly.
+#
+def silently()
+  $VERBOSE, old_verbose = nil, $VERBOSE
+
+  yield
+  
+ensure
+  $VERBOSE = old_verbose
+end
+
+
+DatabaseCleaner.strategy = :transaction
+
+class MiniTest::Spec
+  before :each do
+    test_db = "sqlite3::memory:"
+
+    silently { Oughtve::Database::URI = test_db }
+    DataMapper::Logger.new(STDOUT, :debug) if $VERBOSE
+    DataMapper.setup :default, test_db
+
+    DatabaseCleaner.start
+  end
+
+  after :each do
+    DatabaseCleaner.clean
+  end
+end
 
